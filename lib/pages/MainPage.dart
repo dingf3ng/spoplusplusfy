449ed593e5050c007ca98eff6df398b415b923a0
kaoxi998533 <93632656+kaoxi998533@users.noreply.github.com>
2024-05-24 20:00:51 +0800

    MainPage has basic UI completed

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -1,54 +1,50 @@
 import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spoplusplusfy/main.dart';
 
 final Color goldColour = Color.fromRGBO(255, 232, 163, 1.0);
-List<AssetImage> picList = [];
+List<SvgPicture> picList = [];
 
-class Mainpage extends StatelessWidget {
+class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    picList.add(AssetImage('/assets/image/'));
-    return
-      MaterialApp(
+    return MaterialApp(
         home: Scaffold(
           backgroundColor: Colors.black,
           appBar: AppBar(
             backgroundColor: Colors.black,
             elevation: 0,),
           body:
-          Column(
+          ListView(
+            physics: BouncingScrollPhysics(),
             children: [
-              Text(
-                'Welcome,\nHere Are The Music For\nYou...',
-                style: TextStyle(
-                  fontFamily: 'RobotoMono',
-                  fontSize: 40,
-                  color: goldColour,
-                ),
-              ),
+              Text('Welcome,\nHere Are The Music For\nYou', style: TextStyle(fontSize: 40, color: goldColour),),
               Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  height: 250,
+                child: SizedBox(
                   child: GridView.builder(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.vertical,
                     gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2,
-                      crossAxisSpacing: 20,
+                      crossAxisSpacing: 30,
+                      mainAxisSpacing: 50,
                     ),
                   itemBuilder: (context, index) =>
                   GestureDetector(
                     onTap: () {
-                      Navigator.push(context, 
+                      Navigator.push(context,
                       MaterialPageRoute(builder: (context) => MyApp()));
                     },
                     child: Container(
-                      child: Image(image: picList[index]),
+                      child: picList[index],
                       decoration: BoxDecoration(
                         color: goldColour,
-                        borderRadius: BorderRadius.circular(20),
+                        borderRadius: BorderRadius.circular(50),
                       )
                     ),
                   ),
